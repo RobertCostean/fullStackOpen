e68272a35e8eedf1b27e91bec704625ee6be09a4
RobertCostean <66574496+RobertCostean@users.noreply.github.com>
2023-01-18 07:16:44 +0200

    Delete PoliceStation directory

--- PoliceStation/src/main/java/org/example/interfaces/Administratable.java
+++
@@ -1,8 +0,0 @@
-package org.example.interfaces;
-
-import org.example.classes.Officer;
-
-public interface Administratable {
-    void addOfficer(Officer officer);
-    void removeOfficer(Officer officer);
-}
--- PoliceStation/src/main/java/org/example/interfaces/Investigatable.java
+++
@@ -1,8 +0,0 @@
-package org.example.interfaces;
-
-public interface Investigatable {
-    void addSuspect(String suspect);
-    void removeSuspect(String suspect);
-    void addEvidence(String item);
-    void removeEvidence(String item);
-}
